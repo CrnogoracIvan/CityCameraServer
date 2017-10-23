b908a1f8c3af65b625069805247f0e75165c2561
Jelena Ivezic <jj_ivezic@yahoo.com>
2017-10-23 23:43:22 +0200

    Update localStorageProvider.js

--- services/module_file/providers/localStorageProvider.js
+++ services/module_file/providers/localStorageProvider.js
@@ -19,7 +19,6 @@ var Q = require("q");
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
         console.log("storage destnation function");
-        callback(null, config.file.destination)
         fs.exists(config.file.destination + "/" + moment().format("YYYY_MM_DD"), function (exists) {
             if (exists) {
                 callback(null, config.file.destination + "/" + moment().format("YYYY_MM_DD"));
